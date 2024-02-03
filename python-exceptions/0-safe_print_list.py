3840a84b49222b759c95b2184596221657eb7570
heydarov93 <heyderov.yashar@mail.ru>
2024-02-02 22:19:36 -0800

    if x == 0 case handled

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        i = 0
         for i in range(x):
-            print(my_list[i], end="")        
-    except:
+            print(my_list[i], end="")
+    except IndexError:
         return i
     finally:
+        if i == 0:
+            return 0
         print()
     return i + 1
