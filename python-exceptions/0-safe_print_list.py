a2a07b5e4de69dd7ade63d925342464d0c1f6fb8
heydarov93 <heyderov.yashar@mail.ru>
2024-02-02 22:31:24 -0800

    stdout length increased

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         return i
     finally:
+        print()
         if i == 0:
             return 0
-        print()
     return i + 1
