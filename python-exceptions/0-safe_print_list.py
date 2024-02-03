d01f95e78e812545f8340f5012de7409a187c4bd
heydarov93 <heyderov.yashar@mail.ru>
2024-02-02 23:18:23 -0800

    code optimization

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,10 +4,8 @@ def safe_print_list(my_list=[], x=0):
         i = 0
         for i in range(x):
             print(my_list[i], end="")
+        return i if i == 0 else i + 1
     except IndexError:
         return i
     finally:
         print()
-        if i == 0:
-            return 0
-    return i + 1
