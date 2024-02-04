d543d51de0ba54d81f5c7e2b7ee291a3c25057d9
heydarov93 <heyderov.yashar@mail.ru>
2024-02-04 10:48:59 -0800

    raise error handled

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -15,7 +15,11 @@
 
 def say_my_name(first_name, last_name=""):
     """ print my first and last name """
-    str_error = "first_name must be a string or last_name must be a string"
-    if type(first_name) is not str or type(last_name) is not str:
-        raise TypeError(str_error)
-    print("My name is", first_name, last_name)
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
