43c31e8692c1d7ec5fd5ebf8cc66e71bdfede2d8
heydarov93 <heyderov.yashar@mail.ru>
2024-02-03 23:44:07 -0800

    Square with size. Write a class Square that defines a square.

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines Square class"""
+
+
+class Square:
+    """Square class document
+
+    Attributes:
+        __size (:obj): private size field
+
+    """
+    def __init__(self, size):
+        self.__size = size
