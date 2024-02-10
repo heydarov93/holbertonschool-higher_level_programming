d261dba8e21b309c1335cace7f99e40f203318c4
heydarov93 <heyderov.yashar@mail.ru>
2024-02-10 19:20:04 +0400

    integer_validator method called directly from self

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -9,8 +9,7 @@ class Rectangle(BaseGeometry):
     """Rectangle class based on BaseGeometry base class"""
 
     def __init__(self, width, height):
-        classname = self.__class__
-        classname.integer_validator(self, "width", width)
-        classname.integer_validator(self, "height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
