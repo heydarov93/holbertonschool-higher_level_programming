2d016704c17b8a8e6335eb252a0fdd003e0fb30d
heydarov93 <heyderov.yashar@mail.ru>
2024-02-11 15:49:44 +0400

    implementation of program changed to for loop

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,5 @@ def read_file(filename=""):
     """Opens file, reads it and prints to stdout"""
 
     with open(filename, "r", encoding="utf-8") as afile:
-        print(afile.read().rstrip('\n'), end="")
+        for aline in afile:
+            print(aline, end="")
