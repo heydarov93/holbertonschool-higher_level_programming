3c40eeb78b6278afcb8be2bb6a4f748dc3287877
heydarov93 <heyderov.yashar@mail.ru>
2024-02-11 17:34:12 +0400

    creates an Object from a “JSON file”

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is a '6-load_from_json_file' module"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a 'JSON file'"""
+
+    with open(filename, "r", encoding="utf-8") as afile:
+        return json.loads(afile.read())
