d2a07a58d6d9ad5e0acaed2eecf3c2f8ee5a895e
heydarov93 <heyderov.yashar@mail.ru>
2024-02-11 15:38:53 +0400

    end= removed from inside print. just for test

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Opens file, reads it and prints to stdout"""
 
     with open(filename, "r", encoding="utf-8") as afile:
-        print(afile.read().rstrip(), end="")
+        print(afile.read().rstrip())
