1b06b73d0b7b3b8d857e888a70075ae5053b89a2
heydarov93 <heyderov.yashar@mail.ru>
2024-02-11 17:46:03 +0400

    json.loads method changed to json.load

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -9,4 +9,4 @@ def load_from_json_file(filename):
     """creates an Object from a 'JSON file'"""
 
     with open(filename, "r", encoding="utf-8") as afile:
-        return json.loads(afile.read())
+        return json.load(afile)
