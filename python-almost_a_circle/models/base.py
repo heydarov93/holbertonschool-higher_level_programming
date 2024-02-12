b5680ad2df9fcd85549fedef96a2d049817e718f
heydarov93 <heyderov.yashar@mail.ru>
2024-02-12 13:24:29 +0400

    fixed: E711 comparison to None should be 'if cond is not None:'

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -13,7 +13,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             __class__.__nb_objects += 1
