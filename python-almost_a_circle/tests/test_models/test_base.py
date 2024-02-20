b6837f37b49ca7816cff00c553a566599054193a
heydarov93 <heyderov.yashar@mail.ru>
2024-02-20 23:21:30 +0400

    hmmmmmmm

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -10,16 +10,16 @@ class Test_Base(unittest.TestCase):
 
     def test_id_none(self):
         shape = Base()
-        self.assertEqual(shape.id, 1, "Id should be 1")
+        self.assertEqual(shape.id, 1)
 
     def test_id_negative(self):
         shape = Base(-1)
-        self.assertEqual(shape.id, -1, "Id should be negative")
+        self.assertEqual(shape.id, -1)
 
     def test_id_positive(self):
         id = 5
         shape = Base(id)
-        self.assertEqual(shape.id, id, f"Id should be {id}")
+        self.assertEqual(shape.id, id)
 
     def test_to_json_string(self):
         self.assertEqual(Base.to_json_string(None), '[]')
@@ -30,3 +30,7 @@ class Test_Base(unittest.TestCase):
         self.assertEqual(Base.from_json_string(None), [])
         self.assertEqual(Base.from_json_string(""), [])
         self.assertEqual(Base.from_json_string('[{"age": 30}]'), [{"age": 30}])
+
+
+if __name__ == "__main__":
+    unittest.main()
