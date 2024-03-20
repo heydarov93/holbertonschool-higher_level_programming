c7bc7d21a5b8f93f32f9800df2f95128ff73635c
heydarov93 <heyderov.yashar@mail.ru>
2024-03-20 16:02:34 +0400

    Write a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Computes the score average of all records in the table second_table
+-- The result column name should be average
+-- The database name will be passed as an argument of the mysql command
+SELECT AVG(score) average FROM second_table;
